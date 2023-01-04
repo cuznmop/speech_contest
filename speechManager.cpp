cec797107c2427a990d3b4e1027304447f5e8039
cuznmop <cuzn1984@163.com>
2023-01-04 22:11:48 +0800

    P275

--- speechManager.cpp
+++ speechManager.cpp
@@ -77,13 +77,20 @@ void SpeechManager::startSpeech()
 
     // 3.显示晋级结果
     this->showScore();
+    
     // 第二轮开始比赛
+    this->m_Index++;
 
     // 1.抽签
+    this->speechDraw();
 
     // 2.比赛
+    this->speechContest();
 
     // 3.显示最终结果
+    this->showScore();
+
+    this->m_Index = 1;
 
     // 4.保存最终结果
 }
